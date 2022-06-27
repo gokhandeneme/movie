7f4e93c202c0698c07c774efce2b92fcf25fadab
gokhanDeneme <mgokhant35@gmail.com>
2022-06-27 17:25:56 +0300

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutMovieComponent } from './about-movie/about-movie.component';
+import { MostViewedComponent } from './most-viewed/most-viewed.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutMovieComponent },
+  { path: 'mostVieved', component: MostViewedComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMovieComponent,
+    MostViewedComponent,
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
-export class AppModule { }
+export class AppModule {}
